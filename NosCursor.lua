7e849c6c45c7581e545b1ce5c034f1377be52c83
Beardedrasta <beardedrasta@gmail.com>
2025-02-14 11:55:26 -0500

    first commit

---
+++ NosCursor.lua
@@ -0,0 +1,21 @@
+NosCursor = CreateFrame("Frame", "NosCursorFrame", UIParent)
+
+NosCursor:SetWidth(42)
+NosCursor:SetHeight(42)
+NosCursor:SetFrameStrata("TOOLTIP")
+
+-- Attach a texture to that frame
+local Texture = NosCursor:CreateTexture(nil, "OVERLAY")
+Texture:SetAllPoints(NosCursor)
+Texture:SetTexture("Interface\\AddOns\\NosCursor\\Media\\Circle")
+
+NosCursor:SetScript("OnUpdate", function(self, elapsed)
+    local x, y = GetCursorPosition()
+    local uiScale = UIParent:GetEffectiveScale()
+    x = x / uiScale
+    y = y / uiScale
+
+    this:ClearAllPoints()
+    --this:SetPoint("TOPLEFT", UIParent, "BOTTOMLEFT", x, y)
+    this:SetPoint("CENTER", UIParent, "BOTTOMLEFT", x, y)
+end)
